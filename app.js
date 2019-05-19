3c25ce7b6dfc0fead5539d6e10dc9028bee604cb
Tandara Elena <tandaraelena@yahoo.com>
2019-05-19 14:43:00 +0100

    setup the data locally in a clear object using .map method

--- app.js
+++ app.js
@@ -14,7 +14,25 @@ const productsDOM = document.querySelector(".products-center");
 let cart = [];
 
 // getting the products first locally (from json file), then from contenful
-class Products {}
+class Products {
+  // method
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // display products: is getting the products from Products or local storage class and display them
 class UI {}
@@ -27,4 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // instances for ui and products classes
   const ui = new UI();
   const products = new Products();
+
+  // get all products
+  products.getProducts().then(data => console.log(data));
 });
