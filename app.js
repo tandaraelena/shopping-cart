fdf68a1f396b794681814f12c2a4b0befc7f3aab
Tandara Elena <tandaraelena@yahoo.com>
2019-05-19 18:32:44 +0100

    set the cart values

--- app.js
+++ app.js
@@ -76,16 +76,26 @@ class UI {
         event.target.disabled = true;
         // get product from products
         let cartItem = { ...Storage.getProduct(id), amount: 1 };
-
         // add product to the cart
         cart = [...cart, cartItem];
-        console.log(cart);
         // save cart in local storage
+        Storage.saveCart(cart);
         // set cart values
+        this.setCartValues(cart);
         // display cart item
         // show the cart
       });
     });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
   }
 }
 
@@ -98,6 +108,9 @@ class Storage {
   static getProduct(id) {
     let products = JSON.parse(localStorage.getItem("products"));
     return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 }
 
