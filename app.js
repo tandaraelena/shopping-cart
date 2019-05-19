b504c0f14c1bf39e62da54bcc52b7b5b8e69b039
Tandara Elena <tandaraelena@yahoo.com>
2019-05-19 19:06:01 +0100

    used template literals to interpolate js syntax

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ class UI {
         // set cart values
         this.setCartValues(cart);
         // display cart item
+        this.addCartItem(cartItem);
         // show the cart
       });
     });
@@ -96,6 +97,23 @@ class UI {
     });
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
     cartItems.innerText = itemsTotal;
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+          <img src=${item.image} alt="product" />
+          <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+          </div>
+          <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+          </div>
+    `;
   }
 }
 
